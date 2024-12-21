02b72140908b0ef009983919c383a43d9e2c49f5
MetroWind <chris.corsair@gmail.com>
2024-12-20 17:51:46 -0800

    Use HTTPServer from libmw.

--- src/app.cpp
+++ src/app.cpp
@@ -1,49 +1,22 @@
-#include "mw/error.hpp"
 #define CPPHTTPLIB_FORM_URL_ENCODED_PAYLOAD_MAX_LENGTH 131072
 
 #include <nlohmann/json.hpp>
 #include <inja.hpp>
 #include <httplib.h>
 #include <spdlog/spdlog.h>
-#include <mw/error.hpp>
+
+#include <mw/http_server.hpp>
 
 #include "statics.hpp"
 #include "app.hpp"
 
-#define _ASSIGN_OR_RESPOND_ERROR(tmp, var, val, res)                    \
-    auto tmp = val;                                                     \
-    if(!tmp.has_value())                                                \
-    {                                                                   \
-        if(std::holds_alternative<mw::HTTPError>(tmp.error()))          \
-        {                                                               \
-            const mw::HTTPError& e = std::get<mw::HTTPError>(tmp.error()); \
-            res.status = e.code;                                        \
-            res.set_content(e.msg, "text/plain");                       \
-            return;                                                     \
-        }                                                               \
-        else                                                            \
-        {                                                               \
-            res.status = 500;                                           \
-            res.set_content(std::visit([](const auto& e) { return e.msg; }, \
-                                       tmp.error()),                    \
-                            "text/plain");                              \
-            return;                                                     \
-        }                                                               \
-    }                                                                   \
-    var = std::move(tmp).value()
-
-// Val should be a rvalue.
-#define ASSIGN_OR_RESPOND_ERROR(var, val, res)                          \
-    _ASSIGN_OR_RESPOND_ERROR(_CONCAT_NAMES(assign_or_return_tmp, __COUNTER__), \
-                            var, val, res)
-
 App::App(const Configuration& conf,
          std::unique_ptr<DataSourceInterface> data_source)
-: config(conf),
-    base_url(),
-    templates(),
-    data(std::move(data_source)),
-    should_stop(false)
+        : mw::HTTPServer(conf.listen_address, conf.listen_port),
+          config(conf),
+          base_url(),
+          templates(),
+          data(std::move(data_source))
 {
     auto u = mw::URL::fromStr(conf.base_url);
     if(u.has_value())
@@ -65,8 +38,6 @@ App::App(const Configuration& conf,
             return "Invalid number of url_for() arguments";
         }
     });
-
-    setup();
 }
 
 std::string App::urlFor(const std::string& name, [[maybe_unused]] const std::string& arg) const
@@ -129,24 +100,6 @@ void App::handleRetrieveSave(httplib::Response& res) const
     }
 }
 
-void App::start()
-{
-    spdlog::info("Listening at http://{}:{}/...", config.listen_address,
-                 config.listen_port);
-    server_thread = std::thread([&] {
-        try
-        {
-            server.listen(config.listen_address, config.listen_port);
-        }
-        catch(...)
-        {
-            spdlog::error("Exception when listening.");
-        }
-    });
-    while(!server.is_running());
-    server.wait_until_ready();
-}
-
 void App::setup()
 {
     {
@@ -178,17 +131,4 @@ void App::setup()
     {
         handleRetrieveSave(res);
     });
-
-}
-
-void App::stop()
-{
-    should_stop = true;
-    server.stop();
-}
-
-void App::wait()
-{
-    while(!should_stop && server.is_running());
-    server_thread.join();
 }
--- src/app.hpp
+++ src/app.hpp
@@ -1,21 +1,17 @@
 #pragma once
 
 #include <atomic>
-#include <format>
 #include <memory>
-#include <optional>
 #include <string>
-#include <string_view>
 
 #include <inja.hpp>
-#include <httplib.h>
-#include <spdlog/spdlog.h>
 #include <mw/url.hpp>
+#include <mw/http_server.hpp>
 
 #include "data.hpp"
 #include "config.hpp"
 
-class App
+class App : public mw::HTTPServer
 {
 public:
     App() = delete;
@@ -29,18 +25,11 @@ public:
         const;
     void handleRetrieveSave(httplib::Response& res) const;
 
-    void start();
-    void stop();
-    void wait();
-
 private:
-    void setup();
+    void setup() override;
 
     Configuration config;
     mw::URL base_url;
     inja::Environment templates;
     std::unique_ptr<DataSourceInterface> data;
-    std::atomic<bool> should_stop;
-    std::thread server_thread;
-    httplib::Server server;
 };
